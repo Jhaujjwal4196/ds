5e6f89aa95a68e3e7719e187c9fc7d05f54d3770
Jhaujjwal4196 <ujjwaljh38@gmail.com>
2020-11-12 19:51:39 +0545

    j

--- StringTokenizer.java
+++ StringTokenizer.java
@@ -8,7 +8,7 @@ public class StringTokenizer{
            c=a/b;
            System.out.println(c);
         } catch (ArithmeticException e) {
-            System.out.println(e.getLocalizedMessage()+"error");
+            System.out.println(e.getLocalizedMessage()+" "+"   error");
         }
     }
 
